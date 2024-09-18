323007dce68d736d5303c20157d66fad42239d6b
saba251103 <134624462+saba251103@users.noreply.github.com>
2024-09-18 18:44:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './home';
 import NextPage from './nextpage';
 import Colortest from './colortest';
 import ColorRecommendation from './color';
+import backgroundImage from './background.png' 
 
 function App() {
   return (
     <Router>
-      <div className='App' sx={{}}> 
-      <Routes> 
-        <Route path="/" element={<Home />} />
-        <Route path="/nextpage" element={<NextPage />} />
-        <Route path="/colortest" element={<Colortest />} />
-        <Route path="/color" element={<ColorRecommendation />}/>
-      </Routes>
+      <div 
+        className='App' 
+        style={{
+          backgroundImage: `url(${backgroundImage})`, 
+          backgroundSize: 'cover', 
+          backgroundPosition: 'center', 
+          minHeight: '100vh' // Ensures the background covers the whole viewport
+        }}
+      > 
+        <Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="/nextpage" element={<NextPage />} />
+          <Route path="/colortest" element={<Colortest />} />
+          <Route path="/color" element={<ColorRecommendation />}/>
+        </Routes>
       </div>
     </Router>
   );
